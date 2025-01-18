5caeb46b79773a6424c89d7e3240354ae5e44810
Cande <sanchezgcandelaria@gmail.com>
2025-01-18 17:09:19 -0300

    eslint accepts any

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useSearchParams, useRouter } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
