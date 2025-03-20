afd97d20a4b3a5678a7e8f5ef0366c23ab0631e0
gadelcastillo <gustavodelcastillo2906@hotmail.com>
2025-03-19 22:01:00 -0500

    Update supabaseClient.ts
    se elimina public de las variables de ambiente que exponian informacion sensible

--- src/app/api/supabaseClient.ts
+++ src/app/api/supabaseClient.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 import { Database } from "@/types/database";
 
 export const supabaseClient = createClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
+    process.env.NEXT_SUPABASE_URL as string,
+    process.env.NEXT_SUPABASE_ANON_KEY as string,
 );
