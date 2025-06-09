961c719ea7be2528b2eee2ac7e000bae27ab5dc8
gadelcastillo <gustavodelcastillo2906@hotmail.com>
2025-06-09 18:24:41 -0500

    Creacion de reuniones en google meet

--- src/app/api/profile/profile.ts
+++ src/app/api/profile/profile.ts
@@ -1,6 +1,12 @@
 import { supabaseClient } from "@/app/api/supabaseClient";
 import { SUPABASE_FUNCTIONS, SUPABASE_TABLES } from "@/app/config";
 
+/**
+ * Funcion para buscar el perfil del estudiante usando su ID
+ * Esta funcion utiliza una funcion de supabase para obtener el perfil del estudiante
+ * @param studentId 
+ * @returns 
+ */
 export const getStudentProfileById = async (studentId: number) => {
     try {
         const { data, error } = await supabaseClient
@@ -33,6 +39,11 @@ export const getStudentProfileByUserId = async (userId: string) => {
     }
 }
 
+/**
+ * Funcion para buscar el perfil de usuario usando el id de usuario
+ * @param profileUserId 
+ * @returns 
+ */
 export const getProfileByUserId = async (profileUserId: string) => {
     try {
         const { data, error } = await supabaseClient
@@ -49,3 +60,25 @@ export const getProfileByUserId = async (profileUserId: string) => {
         throw new Error("Error buscando el perfil del profesor en la base de datos");
     }
 }
+
+/**
+ * Funcion generica para obtener el email de un usuario a partir de su id de perfil
+ * @param profileUserId 
+ * @returns 
+ */
+export const getEmailbyUserProfileId = async (profileUserId: number) => {
+    try {
+        const { data, error } = await supabaseClient
+            .from(SUPABASE_TABLES.PROFILES)
+            .select("email")
+            .eq("id", profileUserId);
+        if (error) {
+            console.error(`Error al obtener el email del usuario: ${error.message}`);
+            return null;
+        }
+        return data[0]?.email || null;
+    } catch (e) {
+        console.error(e);
+        throw new Error("Error buscando el email del usuario en la base de datos");
+    }
+};
